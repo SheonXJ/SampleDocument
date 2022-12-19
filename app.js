88fd1fb4ab2f5a366366093c6090781a5b8a1967
SheonXJ <s87462830@gmail.com>
2022-12-19 22:28:28 +0800

    merge: feature/databse

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 // load package
 const express = require('express')
 
+// load env
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+// load database(mongodb)
+require('./config/mongoose')
+
 // setting server
 const PORT = process.env.PORT || 3000
 const app = express()
 
+// setting route
 app.get('/', (req, res) => {
   res.send('hello')
 })
 
+// start server
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
